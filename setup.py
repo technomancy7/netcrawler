5a98e11acea09cba2de439f5d36c1fe01806272e
Kaiser <34060097+Kaiz0r@users.noreply.github.com>
2019-09-14 12:36:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'websearcher',
-  packages = ['websearcher'],
-  version = '19.09.12.5',
+  name = 'webcrawler',
+  packages = ['webcrawler'],
+  version = '19.09.12.6',
   license='gpl-3.0',
   description = 'A collection of tools for searching the internets.',
   author = 'Kaiser',
   author_email = 'technomancer@gmx.com',
-  url = 'https://github.com/Kaiz0r/websearcher',
-  download_url = 'https://github.com/Kaiz0r/websearcher/archive/19.09.12.5.tar.gz',
+  url = 'https://github.com/Kaiz0r/webcrawler',
+  download_url = 'https://github.com/Kaiz0r/webcrawler/archive/19.09.12.6.tar.gz',
   keywords = ['search', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
   install_requires=[
           'beautifulsoup4',
@@ -18,15 +18,12 @@ setup(
     #   3 - Alpha
     #   4 - Beta
     #   5 - Production/Stable
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
 
     # Indicate who your project is intended for
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-
-    # Pick your license as you wish
-    'License :: OSI Approved :: MIT License',
-
+    
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 3',
