00347ed987edbc50e595d1fd70adf3a0f72e02fa
Kaiser <34060097+Kaiz0r@users.noreply.github.com>
2019-12-22 17:19:37 +0000

    Add files via upload

--- netcrawler/netcrawler.py
+++ netcrawler/netcrawler.py
@@ -776,7 +776,7 @@ class Wiki:
 			return en[value.lower()]	
 		except KeyError:
 			if len(en) > 0:
-				return en[choice(en.keys(0))]
+				return en[choice(en.keys())]
 			return {'error': 'Invalid search.'}
 	
 	#Queries wikipedia, returning a list of page summaries.
